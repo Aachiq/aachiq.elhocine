f4c07db48a5806d9099241da84ec8443eb821db0
Think Dev <=>
2025-04-14 18:16:32 +0100

    fix changes website version to deploy githubAction

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -4,7 +4,7 @@ import './ProjectContainer.css'
 const ProjectContainer = ({ project }) => (
   <div className='project' style={{ margin: '10px' }}>
     <h3 className='project-title'>{project.name}</h3>
-    <img src={`/cleanfolio/images/${project.image}`} alt='project_img' width='100%' />
+    <img src={`/personnal-website/images/${project.image}`} alt='project_img' width='100%' />
     <p className='project__description'>{project.description}</p>
     {project.stack && (
       <ul className='project__stack'>

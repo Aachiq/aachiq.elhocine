ec7f12290390bdf6afd698137aacfbccc2d2ab2f
Think Dev <=>
2025-04-17 21:39:12 +0100

    remove stack data

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,42 +21,42 @@ const projects = [
     name: 'Afric Trust',
     image: 'afric-trust.png',
     description: 'A platform offering financial services and investment opportunities across Africa.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     linkPreview: 'https://www.africtrust.com',
   },
   {
     name: 'Crédit Habitat',
     image: 'credit-habitat.png',
     description: 'Providing home loans and real estate investment solutions in Morocco.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     linkPreview: 'https://www.credithabitat.ma',
   },
   {
     name: 'Daba Transfert Platform',
     image: 'daba-transfert.png',
     description: 'A platform for fast and secure money transfers across regions.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     linkPreview: 'https://www.dabatransfer.com',
   },
   {
     name: 'Daba Transfert Mobile',
     image: 'daba-transfert-mobile.png',
     description: 'Mobile app for easy and secure money transfers.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     linkPreview: 'https://play.google.com/store/apps/details?id=co.dabasfer',
   },
   {
     name: 'Credit Entreprise',
     image: 'credit-entreprises.png',
     description: 'Financing solutions for businesses to manage cash flow and growth.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     linkPreview: 'https://www.creditbusinessonline.ma/',
   },
   {
     name: 'Shems AlMadina Immobilier',
     image: 'shems-almadina.png',
     description: 'Real estate platform for buying, selling, and renting properties in Morocco.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     linkPreview: 'https://shemsalmadina.ma/',
   },
 ]
@@ -67,42 +67,42 @@ const personnalProjects = [
     name: 'Youtube Platform',
     image: 'youtube.gif.png',
     description: 'A platform for video sharing, similar to YouTube, built with React and TypeScript.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     // linkPreview: 'https://www.linkedin.com/feed/update/urn:li:activity:7211066531378593793?updateEntityUrn=urn%3Ali%3Afs_updateV2%3A%28urn%3Ali%3Aactivity%3A7211066531378593793%2CFEED_DETAIL%2CEMPTY%2CDEFAULT%2Cfalse%29&originTrackingId=LEn%2FGSqIRzeK5IXY2puaPA%3D%3D&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recent_activity_content_view%3BclACZuvWS520ORKaOCejsg%3D%3D'
   },
   {
     name: 'Twitter Clone',
     image: 'twitter2.png',
     description: 'A dark theme version of Twitter, featuring a modern design and user-friendly interface.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     // linkPreview: 'https://www.linkedin.com/feed/update/urn:li:activity:7219077072718020609?updateEntityUrn=urn%3Ali%3Afs_updateV2%3A%28urn%3Ali%3Aactivity%3A7219077072718020609%2CFEED_DETAIL%2CEMPTY%2CDEFAULT%2Cfalse%29&originTrackingId=GW4TSyBvSd6hgYsd%2ByO%2Bbw%3D%3D&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recent_activity_content_view%3BclACZuvWS520ORKaOCejsg%3D%3D'
   },
   {
     name: 'FullStack Social Media React-Node-MongoDB',
     image: 'socialMedia.gif.png',
     description: 'A full-stack social media application using React, Node.js, and MongoDB for user interactions.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     // linkPreview: 'https://www.linkedin.com/feed/update/urn:li:activity:7251535650460438528?updateEntityUrn=urn%3Ali%3Afs_updateV2%3A%28urn%3Ali%3Aactivity%3A7251535650460438528%2CFEED_DETAIL%2CEMPTY%2CDEFAULT%2Cfalse%29&originTrackingId=99%2BtTKuyR9W8G%2Bf5CLM8JA%3D%3D&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BTwziQ33uQmKYU327siYHwQ%3D%3D'
   },
   {
     name: 'FullStack Ecommerce With Admin Dashboard',
     image: 'ecom1.png',
     description: 'An e-commerce platform with a full-stack setup, including an admin dashboard for managing products and orders.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     // linkPreview: 'https://tinyurl.com/z6xjrnvf'
   },
   {
     name: 'LinkedIn Site Using React With Styled Component ',
     image: 'linkedIn1.png',
     description: 'An e-commerce platform with a full-stack setup, including an admin dashboard for managing products and orders.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     linkPreview: 'https://shemsalmadina.ma/'
   },
   {
     name: 'More Projects ...',
     image: 'faceb1.png',
     description: 'An e-commerce platform with a full-stack setup, including an admin dashboard for managing products and orders.',
-    stack: ['SASS', 'TypeScript', 'React'],
+    // stack: ['SASS', 'TypeScript', 'React'],
     // linkPreview: 'https://www.linkedin.com/feed/update/urn:li:activity:7301235982975029248?updateEntityUrn=urn%3Ali%3Afs_updateV2%3A%28urn%3Ali%3Aactivity%3A7301235982975029248%2CFEED_DETAIL%2CEMPTY%2CDEFAULT%2Cfalse%29&originTrackingId=cKGhQOekSrS5HPI78BFOJQ%3D%3D&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recent_activity_content_view%3BclACZuvWS520ORKaOCejsg%3D%3D'
   },
 ]
